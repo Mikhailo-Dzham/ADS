830fd18f3d5aa2f3007d4e52ecdc5cac2c4cabb4
Mykhailo <dzhammykhailo@knu.ua>
2026-04-03 13:13:38 +0300

    Some classwork

---
+++ ClassWork/CW14/t14_4.py
@@ -0,0 +1,68 @@
+class Queue():
+    def __init__(self, capacity=100):
+        self.data = [0] * capacity
+        self.capacity = capacity
+        self.first = None
+        self.last = None
+        self.__size = 0
+
+    def push(self, n):
+        if self.first == None:
+            self.first = 0
+            self.last = 0
+        else:
+            self.last = (self.last + 1) % self.capacity
+        self.data[self.last] = n
+        self.__size += 1
+        print("ok")
+
+    def pop(self):
+        val = self.data[self.first]
+        if self.first == self.last:
+            self.first = None
+            self.last = None
+        else:
+            self.first = (self.first + 1) % self.capacity
+        self.__size -= 1
+        print(val)
+        return val
+
+    def front(self):
+        val = self.data[self.first]
+        print(val)
+        return val
+
+    def size(self):
+        print(self.__size)
+        return self.__size
+
+    def clear(self):
+        self.first = None
+        self.last = None
+        self.__size = 0
+        print("ok")
+
+
+def exit():
+    print("bye")
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    import sys
+
+    q = Queue()
+    DICT = {
+        "push": q.push,
+        "pop": q.pop,
+        "front": q.front,
+        "size": q.size,
+        "clear": q.clear,
+        "exit": exit
+    }
+    for line in sys.stdin:
+        command = line.split()
+        if len(command) == 2:
+            DICT[command[0]](command[1])
+        else:
+            DICT[command[0]]()
